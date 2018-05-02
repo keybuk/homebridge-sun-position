cceacccacd164bd38ddbbaf998d61cf9874779c4
Scott James Remnant <scott@netsplit.com>
2018-05-01 21:12:04 -0700

    Do the characteristics in a less hacky way

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var suncalc = require('suncalc');
+var inherits = require('util').inherits,
+	suncalc = require('suncalc');
 
 let SERVICE_UUID  = 'a8330e43-f6f4-4a7f-80ae-6b2915cc4569';
 let ALTITUDE_UUID = 'a8af30e7-5c8e-43bf-bb21-3c1343229260';
@@ -10,10 +11,40 @@ module.exports = function(homebridge) {
 	Accessory = homebridge.hap.Accessory;
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
-	UUIDGen = homebridge.hap.uuid;
 
 	homebridge.registerAccessory('homebridge-sun-position', 'SunPosition', SunPositionAccessory);
+
+	AltitudeCharacteristic = function() {
+		Characteristic.call(this, 'Altitude', ALTITUDE_UUID);
+
+		this.setProps({
+	    	format: Characteristic.Formats.FLOAT,
+	    	unit: Characteristic.Units.ARC_DEGREE,
+	    	minValue: -90,
+	    	maxValue: 90,
+	    	minStep: 0.1,
+	    	perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+	    });
+		this.value = this.getDefaultValue();
+	}
+	inherits(AltitudeCharacteristic, Characteristic);
+
+	AzimuthCharacteristic = function() {
+		Characteristic.call(this, 'Azimuth', AZIMUTH_UUID);
+
+		this.setProps({
+	    	format: Characteristic.Formats.FLOAT,
+	    	unit: Characteristic.Units.ARC_DEGREE,
+	    	minValue: 0,
+	    	maxValue: 360,
+	    	minStep: 0.1,
+	    	perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+	    });
+		this.value = this.getDefaultValue();
+	}
+	inherits(AzimuthCharacteristic, Characteristic);
 }
+
 
 function SunPositionAccessory(log, config) {
 	this.log = log;
@@ -26,28 +57,21 @@ function SunPositionAccessory(log, config) {
 	this.location = config.location;
 }
 
+SunPositionAccessory.prototype.identify = function(callback) {
+	this.log("Identify");
+	callback();
+}
+
 SunPositionAccessory.prototype.getServices = function() {
+	this.informationService = new Service.AccessoryInformation();
+	this.informationService
+		.setCharacteristic(Characteristic.Manufacturer, "github.com keybuk")
+		.setCharacteristic(Characteristic.Model, "Sun Position")
+		.setCharacteristic(Characteristic.SerialNumber, "");
+
     this.service = new Service("Sun Position", SERVICE_UUID);
-
-    this.service.addCharacteristic(new Characteristic("Altitude", ALTITUDE_UUID))
-    	.setProps({
-    		format: Characteristic.Formats.FLOAT,
-    		unit: Characteristic.Units.ARC_DEGREE,
-    		minValue: -90,
-    		maxValue: 90,
-    		minStep: 0.1,
-    		perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-    	});
-
-    this.service.addCharacteristic(new Characteristic("Azimuth", AZIMUTH_UUID))
-    	.setProps({
-    		format: Characteristic.Formats.FLOAT,
-    		unit: Characteristic.Units.ARC_DEGREE,
-    		minValue: 0,
-    		maxValue: 360,
-    		minStep: 0.1,
-    		perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-    	});
+    this.service.addCharacteristic(AltitudeCharacteristic);
+    this.service.addCharacteristic(AzimuthCharacteristic);
 
     this.updatePosition();
 
@@ -62,8 +86,8 @@ SunPositionAccessory.prototype.updatePosition = function() {
 
 	this.log("Sun is " + altitude + " high at " + azimuth);
 
-	this.service.setCharacteristic("Altitude", altitude);
-	this.service.setCharacteristic("Azimuth", azimuth);
+	this.service.setCharacteristic(AltitudeCharacteristic, altitude);
+	this.service.setCharacteristic(AzimuthCharacteristic, azimuth);
 
 	setTimeout(this.updatePosition.bind(this), UpdatePeriod);
 }
